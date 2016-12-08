969a5cead7465c191251a1dfdca5636d9780bab4
Leo428 <huzheyuan428@gmail.com>
2016-12-08 13:20:39 -0800

    end version
    cute!

--- src/console/CLauncher.java
+++ src/console/CLauncher.java
@@ -1,11 +1,25 @@
 package console;
 
+import java.util.Scanner;
+
+import controller.GameController;
+
 /*
  * This is the console launcher for the game 
  * This is also an useless line of comment ...... 
  */
 public class CLauncher {
 	public static void main(String[] args){
-		
+		Scanner scanner = new Scanner(System.in);
+		GameController gameController = new GameController();
+//		String endWord = ""; 
+//		gameController.reset();
+//		do{
+			gameController.starter();
+			if(gameController.isSingleMode) gameController.singlePlayerGame();
+			else gameController.twoPlayersGame();
+//			System.out.println("Enter end to quit the game or press enter to continue!");
+//			endWord = scanner.nextLine(); 
+//		}while(!endWord.equalsIgnoreCase("end"));
 	}
 }
